73b5abd849a6ba4e8f5d55f2833f2399aa0917ba
Valerio <valerio.marchitelli@student.univaq.it>
2019-11-19 18:08:28 +0100

    User model, checkIfUserIsAdmin, create user

--- src/Model/User.java
+++ src/Model/User.java
@@ -1,7 +1,14 @@
 package Model;
 
+import Controller.DBManager;
+
 public class User {
-    private String user,password,type;
+    private String user,password;
+    private boolean type;
+
+    public User(String user, String password, boolean type){
+        DBManager.addUser(user,password,type);
+    }
 
     public String getUser() {
         return user;
