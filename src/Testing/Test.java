01eb3269dc66ef990dbfe70765eae2a14e8b40cc
Cico96 <40207930+Cico96@users.noreply.github.com>
2019-11-19 18:51:09 +0100

    prova

--- src/Testing/Test.java
+++ src/Testing/Test.java
@@ -99,6 +99,7 @@ public class Test extends Application {
         //TollBoth.setName("TE00","TE0");
         //TollBoth.setKM("TE0",0);
         //TollBoth.delTollBooth("TE0");
+        //test
         launch(args);
     }
 }
