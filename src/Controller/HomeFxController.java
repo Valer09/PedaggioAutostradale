bc32ef2b1223334c864f2be4870f617631c6c39b
Cico96 <40207930+Cico96@users.noreply.github.com>
2019-11-20 22:44:25 +0100

    gui home e calcola pedaggio

--- src/Controller/HomeFxController.java
+++ src/Controller/HomeFxController.java
@@ -1,14 +1,35 @@
 package Controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class HomeFxController implements Initializable {
+    @FXML
+    Button op;
 
+    private void switchScene(javafx.event.ActionEvent event) {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/sample.fxml"));
+        Stage stage = (Stage) op.getScene().getWindow();
+        Scene scene = null;
+        try {
+            scene = new Scene(loader.load());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        stage.setScene(scene);
+
+    }
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        op.setOnAction(this::switchScene);
+
 
     }
 
