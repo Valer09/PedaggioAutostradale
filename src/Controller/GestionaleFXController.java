d1aa415191619e39b46d75b8d29dad13cba78bb4
Valerio <valerio.marchitelli@student.univaq.it>
2019-11-25 18:07:57 +0100

    GestionaleFXController

--- src/Controller/GestionaleFXController.java
+++ src/Controller/GestionaleFXController.java
@@ -1,14 +1,18 @@
 package Controller;
 
+import Model.Highway;
 import javafx.fxml.Initializable;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class GestionaleFXController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        ArrayList <Highway> highways = DBManager.getHighways();
+        System.out.println(highways);
 
     }
 }
